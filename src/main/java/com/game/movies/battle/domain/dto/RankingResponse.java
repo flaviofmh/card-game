ea684c772e0921b9961c5475c718fea96a98f6a2
Flavio Honorato <flavio_fmh@hotmail.com>
2024-05-12 20:13:01 +0100

    TAREFA-10 | Criado logica de ranking, refatorado codigo para uns problemas de negocio com match do user

---
+++ src/main/java/com/game/movies/battle/domain/dto/RankingResponse.java
@@ -0,0 +1,11 @@
+package com.game.movies.battle.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class RankingResponse {
+
+    private String fullName;
+    private Double totalScore;
+
+}
--- src/main/java/com/game/movies/battle/domain/service/RoundService.java
+++ src/main/java/com/game/movies/battle/domain/service/RoundService.java
@@ -5,6 +5,7 @@ import com.game.movies.battle.api.exceptionhandler.exception.ExistsGameException
 import com.game.movies.battle.api.exceptionhandler.exception.RoundHasFinished;
 import com.game.movies.battle.domain.entity.Player;
 import com.game.movies.battle.domain.entity.Round;
+import com.game.movies.battle.domain.entity.SequenceMoviesRound;
 import com.game.movies.battle.domain.repository.RoundRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -45,8 +46,22 @@ public class RoundService {
         Round roundCurrent = roundRepository.findByIdAndFinishedFalse(roundId).orElseThrow(() -> new ExistsGameException(
                 String.format(MESSAGE_ROUND_ID, roundId)));
 
+        var totalScore = calcTotalScore(roundCurrent);
+
         roundCurrent.setFinished(true);
+        roundCurrent.setTotalScore(totalScore);
         roundRepository.save(roundCurrent);
+    }
+
+    private Double calcTotalScore(Round roundCurrent) {
+
+        var totalQuestions = (double) roundCurrent.getSequenceMoviesRounds().size();
+        var totalRight = roundCurrent.getSequenceMoviesRounds().stream().mapToDouble(SequenceMoviesRound::getScore).sum();
+
+        var percentRight = (totalRight / totalQuestions) * 100;
+        var totalFinal = percentRight * totalRight;
+
+        return totalFinal;
     }
 
     public Round getRoundById(Long roundId) {
@@ -56,17 +71,28 @@ public class RoundService {
         return round;
     }
 
+    public Round getRoundByIdAndPlayerId(Long roundId, Long playerId) {
+        Round round = roundRepository.findByIdAndPlayerId(roundId, playerId).orElseThrow(() -> new EntityNotFoundException(
+                String.format(MESSAGE_ROUND_ID, roundId)));
+
+        return round;
+    }
+
     public List<Round> getAll() {
         return roundRepository.findAll();
+    }
+
+    public List<Round> getAllRoundsOrderedByScore() {
+        return roundRepository.findByOrderByTotalScoreDesc();
     }
 
     public void updateAttempts(Round currentRound, Long sequenceMoviesRoundId) {
         var sequenceMoviesRound = currentRound.getSequenceMoviesRounds().stream().filter(movies -> movies.getId().equals(sequenceMoviesRoundId)).findFirst();
         if (sequenceMoviesRound.isPresent()) {
-            if (sequenceMoviesRound.get().isPendingAnswer()) {
+            if (sequenceMoviesRound.get().getScore().equals(0)) {
                 currentRound.setAttempts(currentRound.getAttempts()+1);
                 if (currentRound.getAttempts().equals(3)) {
-                    currentRound.setFinished(true);
+                    stop(currentRound.getId());
                 }
             }
         }
